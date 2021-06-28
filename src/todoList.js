71890876b3551899441a01b93ad6c93c139b0efd
lumsdnb <jeremy@lums.io>
2021-06-28 15:19:46 +0200

    Add todos & get dom ready list

--- src/todoList.js
+++ src/todoList.js
@@ -1,9 +1,29 @@
 import todoItem from './todoItem';
 
 class todoList {
-  constructor(listName, todos) {
+  constructor(listName) {
     this.listName = listName;
-    this.todos = todos;
+    this.todos = [];
+  }
+
+  addTodo(item) {
+    const t = new todoItem(item, '', '2021-04-17', '!!');
+    this.todos.push(t);
+  }
+
+  get domElement() {
+    const el = document.createElement('div');
+    for (let i = 0; i < this.todos.length; i++) {
+      const item = this.todos[i];
+      el.append(item.domElement);
+      console.log(item);
+    }
+
+    const title = document.createElement('h1');
+    title.innerHTML = this.listName;
+    el.classList.add('todo-list');
+    el.prepend(title);
+    return el;
   }
 }
 
