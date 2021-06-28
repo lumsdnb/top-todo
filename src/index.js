de6a7e5b05f1866bcb28ebf574635800a3cbda50
lumsdnb <jeremy@lums.io>
2021-06-28 15:20:24 +0200

    Add placeholder todos & btn event

--- src/index.js
+++ src/index.js
@@ -8,7 +8,17 @@ function component() {
   const element = document.createElement('div');
   element.classList.add('card');
 
+  const addTodo = (e) => {
+    console.log('open modal');
+  };
+
   const defaultList = new todoList('default');
+  defaultList.addTodo('TODOd title');
+  defaultList.addTodo('another one');
+  defaultList.addTodo('another one');
+  defaultList.addTodo('another one');
+  defaultList.addTodo('another one');
+  defaultList.addTodo('another one');
 
   const addBtn = document.createElement('button');
   addBtn.classList.add('add-todo-button');
@@ -16,6 +26,7 @@ function component() {
 
   container.parentElement.prepend(navBar());
   element.append(defaultList.domElement, addBtn);
+  addBtn.addEventListener('click', addTodo);
 
   return element;
 }
